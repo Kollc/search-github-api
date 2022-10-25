a0d57ea1258270eb641849ff2eda8f53ee7a2d5b
Vladimir Yakovlev <mr.kollc@list.ru>
2022-10-25 21:38:51 +0300

    delete token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,3 @@
 import { Octokit } from "octokit";
 
-export const octokit = new Octokit({
-  auth: "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",
-});
+export const octokit = new Octokit({});
